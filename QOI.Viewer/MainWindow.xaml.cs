d8228d47f46c56cb83dc7730a15c83dc4244dda2
TollyH <ptolemyhill@sky.com>
2023-07-27 19:44:16 +0100

    Add option to save as PNG in viewer

--- QOI.Viewer/MainWindow.xaml.cs
+++ QOI.Viewer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Win32;
+using System.IO;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace QOI.Viewer
 {
@@ -28,6 +30,32 @@ namespace QOI.Viewer
 
             QOIImage image = QOIDecoder.DecodeImageFile(fileDialog.FileName);
             imageView.Source = image.ConvertToBitmapImage();
+        }
+
+        private void SaveItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (imageView.Source is null or not BitmapImage)
+            {
+                return;
+            }
+
+            SaveFileDialog fileDialog = new()
+            {
+                CheckPathExists = true,
+                DefaultExt = ".png",
+                Filter = "PNG Image File|*.png",
+                AddExtension = true
+            };
+
+            if (!fileDialog.ShowDialog() ?? true)
+            {
+                return;
+            }
+
+            PngBitmapEncoder encoder = new();
+            encoder.Frames.Add(BitmapFrame.Create((BitmapImage)imageView.Source));
+            using FileStream fileStream = new(fileDialog.FileName, FileMode.Create);
+            encoder.Save(fileStream);
         }
     }
 }
