99e21cc8d964274a585cdefe5ddc46cd4a5cefea
TollyH <ptolemyhill@sky.com>
2023-07-27 18:33:51 +0100

    Basic initial viewer implementation

---
+++ QOI.Viewer/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace QOI.Viewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new()
+            {
+                CheckFileExists = true,
+                Filter = "QOI Image File|*.qoi"
+            };
+
+            if (!fileDialog.ShowDialog() ?? true)
+            {
+                return;
+            }
+
+            QOIImage image = QOIDecoder.DecodeImageFile(fileDialog.FileName);
+            imageView.Source = image.ConvertToBitmapImage();
+        }
+    }
+}
---
+++ QOI.Viewer/QOIImageConverter.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace QOI.Viewer
+{
+    public static class QOIImageConverter
+    {
+        public static BitmapImage ConvertToBitmapImage(this QOIImage image)
+        {
+            int width = (int)image.Width;
+            int height = (int)image.Height;
+            using Bitmap bitmap = new(width, height, PixelFormat.Format32bppArgb);
+            BitmapData bmpData = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+            // Copy pixel array to new Bitmap object
+            unsafe
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    byte* row = (byte*)bmpData.Scan0 + (y * bmpData.Stride);
+                    for (int x = 0; x < width; x++)
+                    {
+                        Pixel pixel = image.Pixels[(y * width) + x];
+                        row[x * 4] = pixel.Blue;
+                        row[(x * 4) + 1] = pixel.Green;
+                        row[(x * 4) + 2] = pixel.Red;
+                        row[(x * 4) + 3] = pixel.Alpha;
+                    }
+                }
+            }
+
+            bitmap.UnlockBits(bmpData);
+
+            // Convert Bitmap to BitmapImage for use with WPF
+            using MemoryStream stream = new();
+            bitmap.Save(stream, ImageFormat.Png);
+
+            BitmapImage bitmapImage = new();
+            bitmapImage.BeginInit();
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.StreamSource = stream;
+            bitmapImage.EndInit();
+            bitmapImage.Freeze();
+            return bitmapImage;
+        }
+    }
+}
