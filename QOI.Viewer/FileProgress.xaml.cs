a1a9a905e951f7c6802f1d6022af7b07fa21f81f
TollyH <ptolemyhill@sky.com>
2023-07-30 00:04:20 +0100

    File progress user control

---
+++ QOI.Viewer/FileProgress.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace QOI.Viewer
+{
+    /// <summary>
+    /// Interaction logic for FileProgress.xaml
+    /// </summary>
+    public partial class FileProgress : UserControl
+    {
+        public static readonly DependencyProperty FilenameProperty =
+            DependencyProperty.Register("Filename", typeof(string), typeof(FileProgress));
+        public string Filename
+        {
+            get => (string)GetValue(FilenameProperty);
+            set
+            {
+                fileNameLabel.Text = value;
+                SetValue(FilenameProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty IsCompleteProperty =
+            DependencyProperty.Register("IsComplete", typeof(bool), typeof(FileProgress));
+        public bool IsComplete
+        {
+            get => (bool)GetValue(IsCompleteProperty);
+            set
+            {
+                finishedIndicator.Fill = value ? Brushes.Green : null;
+                SetValue(IsCompleteProperty, value);
+            }
+        }
+
+        public FileProgress()
+        {
+            InitializeComponent();
+        }
+    }
+}
